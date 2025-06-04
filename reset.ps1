314fe26cc98f8869eaef10c3b7fbe70cd30f3360
Adrien-LACROIX <lacx.adrien@gmail.com>
2025-06-04 19:22:37 +0200

    run main.go

--- reset.ps1
+++ reset.ps1
@@ -8,13 +8,15 @@ docker-compose down -v
 Write-Host "Reconstruction et démarrage des conteneurs..."
 docker-compose up --build -d
 
-Write-Host "Attente du démarrage de Keycloak (30s)..."
-Start-Sleep -Seconds 30
+##Write-Host "Attente du démarrage de Keycloak (30s)..."
+##Start-Sleep -Seconds 30
 
 Write-Host 'Initialisation de Keycloak (realm, client, user)...'
 .\install-keycloak.ps1
 
 Write-Host "Ouverture de l application dans le navigateur..."
+cd app/
+go run ./main.go
 Start-Process "http://localhost:8080"
 
 Write-Host "Environnement pret à l emploi avec interface ouverte..."
